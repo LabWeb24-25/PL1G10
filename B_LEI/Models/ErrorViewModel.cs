48e7d39ee0fd9c4a4f82af7b83f2cd21782728be
Daniel <Daniel@DESKTOP-I9HVNEG>
2024-12-25 17:08:27 +0000

    ok

--- B_LEI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace B_LEI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
