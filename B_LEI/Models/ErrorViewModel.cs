9d006e109031db08deba20fbf3a16f1fbec3deaf
Daniel <Daniel@DESKTOP-I9HVNEG>
2024-12-25 02:24:55 +0000

    Removed unnecessary folders

--- B_LEI/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace B_LEI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
