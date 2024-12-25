5cd681b6489fc335e120e24f4247c613f1cb2b88
LFRA7 <lucasfralves89@gmail.com>
2024-12-25 11:26:49 +0000

    projeto criado

---
+++ B_LEI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace B_LEI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
