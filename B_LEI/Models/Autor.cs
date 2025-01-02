9a6887ff24619ff63eeba25171975def5b53b622
Daniel <Daniel@DESKTOP-I9HVNEG>
2025-01-02 18:23:01 +0000

    okk

--- B_LEI/Models/Autor.cs
+++ B_LEI/Models/Autor.cs
@@ -6,7 +6,7 @@
         public string? Nome { get; set; }
         public string? Foto { get; set; }
 
-        public int LivroId { get; set; }
+       
         public ICollection<Livro> Livros { get; set; } = new List<Livro>();
     }
 
